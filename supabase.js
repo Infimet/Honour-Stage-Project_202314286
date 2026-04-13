cc2db1886076ebae98952cb116d9892b77cc090d
Infimet <r.miah-2022@hull.ac.uk>
2026-04-13 14:42:26 +0100

    Added Supabase initial Integration
    UI changes to come down the line actually-- after trying to add them earlier, I kept getting issues, so i'm putting that on the backburner. I'm now going to work on adding a stable database ussing a SAAS in Supabase to add protected user classification. (necessary before i continue working on the level select, and fleshing that feature out)

---
+++ supabase.js
@@ -0,0 +1,6 @@
+const SUPABASE_URL     = 'https://idhvfxbkfljtwjfvpulu.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkaHZmeGJrZmxqdHdqZnZwdWx1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwNjU0NjAsImV4cCI6MjA5MTY0MTQ2MH0._SEWNgZPwGKfBwHiGbT2kG8Uq4Jcbq-1uTS8DJC1IAI';
+
+// the cdn script (loaded in index.html) exposes a global `supabase` object
+const { createClient } = supabase;
+const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
